6cc5f51f8c304578520967d088ee317d54a25207
sodiasm <sodiasm@duck.com>
2026-03-16 22:11:02 +0800

    Revert "feat: remaining ui components (table, tabs, textarea, scroll-area, switch, popover) + components.json + drizzle/schema.ts (users table removed)"
    This reverts commit 0738e90344bfde2623a58bc5c5ef0f6f191637b5.

--- client/src/components/ui/switch.tsx
+++ client/src/components/ui/switch.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import * as SwitchPrimitive from "@radix-ui/react-switch";
+
 import { cn } from "@/lib/utils";
 
-function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+function Switch({
+  className,
+  ...props
+}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
@@ -14,7 +18,9 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
-        className="bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+        className={cn(
+          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+        )}
       />
     </SwitchPrimitive.Root>
   );
